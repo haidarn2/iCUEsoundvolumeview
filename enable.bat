0b9d632c51da9ffb8b28e0f8988180009dff7938
Nadeem Haidar <nadeem_haidar@hotmail.com>
2021-10-20 21:42:19 -0400

    Add files via upload

--- enable.bat
+++ enable.bat
@@ -1,4 +1,4 @@
-C:\Users\Nadeem\Downloads\soundvolumeview-x64\SoundVolumeView.exe /SetPlaybackThroughDevice "Microphone" "Realtek HD Audio 2nd output"
-C:\Users\Nadeem\Downloads\soundvolumeview-x64\SoundVolumeView.exe /SetListenToThisDevice "Microphone" 1
-C:\Users\Nadeem\Downloads\soundvolumeview-x64\SoundVolumeView.exe /SetPlaybackThroughDevice "{0.0.1.00000000}.{f5da3f17-af16-4f47-87b5-03c8dba2e5e4}" "Default Playback Device"
-C:\Users\Nadeem\Downloads\soundvolumeview-x64\SoundVolumeView.exe /SetListenToThisDevice "{0.0.1.00000000}.{f5da3f17-af16-4f47-87b5-03c8dba2e5e4}" 1
+start "" C:\Users\Nadeem\Downloads\soundvolumeview-x64\SoundVolumeView.exe /SetPlaybackThroughDevice "Microphone" "Realtek HD Audio 2nd output"
+start "" C:\Users\Nadeem\Downloads\soundvolumeview-x64\SoundVolumeView.exe /SetListenToThisDevice "Microphone" 1
+start "" C:\Users\Nadeem\Downloads\soundvolumeview-x64\SoundVolumeView.exe /SetPlaybackThroughDevice "{0.0.1.00000000}.{f5da3f17-af16-4f47-87b5-03c8dba2e5e4}" "Default Playback Device"
+start "" C:\Users\Nadeem\Downloads\soundvolumeview-x64\SoundVolumeView.exe /SetListenToThisDevice "{0.0.1.00000000}.{f5da3f17-af16-4f47-87b5-03c8dba2e5e4}" 1
