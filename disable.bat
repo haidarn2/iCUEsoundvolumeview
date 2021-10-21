2a32cc78e614eb1f19b652d28b760a6646e1b139
Nadeem Haidar <nadeem_haidar@hotmail.com>
2021-10-20 21:30:33 -0400

    Add files via upload

--- disable.bat
+++ disable.bat
@@ -1,1 +1,2 @@
 C:\Users\Nadeem\Downloads\soundvolumeview-x64\SoundVolumeView.exe /SetListenToThisDevice "Microphone" 0
+C:\Users\Nadeem\Downloads\soundvolumeview-x64\SoundVolumeView.exe /SetListenToThisDevice "{0.0.1.00000000}.{f5da3f17-af16-4f47-87b5-03c8dba2e5e4}" 0
